a6c09b034bee74338fa05388810e2c11a66d5f39
aellanglet <ael.langlet@hetic.net>
2017-10-30 12:31:44 +0100

    Just a cron program

--- readme.md
+++ readme.md
@@ -4,8 +4,12 @@ Affichage de l'heure de la pause dans la page console et la page html.
 
 Boucle temporelle: Toutes les 5 minutes on check si c'est l'heure de la pause (10 h 30).
 
-Démarrer le server node et regarder le localhost sur le port 3000 ou bien regarder les logs dans la console.
-
 Effectué avec le package npm : "cron".
 
 On apprécie l'utf-8 non géré !
+
+INSTRUCTIONS :
+
+Démarrer le server node dans le terminal et regarder le localhost sur le port 3000 ou bien regarder les logs dans la console.
+
+Il faut auparavant aller sur votre localhost pour que le programme se lance.
--- server.js
+++ server.js
@@ -7,43 +7,40 @@ const bodyParser = require('body-parser');
 
 const CronJob = require('cron').CronJob;
 
-console.log("Le programme est lancé ! " + new Date());
+console.log("Le programme n'a pas encore été démarré : " + new Date());
 
 app.get("/", (req, res) => {
+  console.log("Le programme a été démarré : " + new Date());
   res.write('<h1>Progamme LaPauseHetic !</h1>' + '<br>' + '<h2>Le programme est lancé ! ' + new Date() + '</h2>');
 
-  if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
-    res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>');
-  } else {
-    res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>');
-  }
+  new CronJob('*/5 * * * *', () => {
+
+      if ((new Date().getHours() >= 10) == true) {
+
+        if ((new Date().getHours() >= 10) == true && (new Date().getMinutes() >= 30) == true) {
+          console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
+          console.log("Il est : " + new Date());
+          res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>' + '<script>location.reload()</script>');
+        } else {
+          res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<script>location.reload()</script>');
+          console.log("Ce n'est pas encore l'heure de la pause.");
+          console.log("Il est : " + new Date());
+        }
+
+      } else {
+
+        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<script>location.reload()</script>');
+        console.log("Ce n'est pas encore l'heure de la pause.");
+        console.log("Il est : " + new Date());
+
+      }
+
+    }, () => {},
+    true
+  );
 
 });
 
-new CronJob('*/5 * * * *', () => {
-
-    if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
-      console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
-      console.log("Il est : " + new Date());
-    } else {
-      console.log("Ce n'est pas encore l'heure de la pause.");
-      console.log("Il est : " + new Date());
-    }
-
-    app.get("/", (req, res) => {
-
-      if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
-        res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>' + '<script>location.reload()</script>');
-      } else {
-        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<script>location.reload()</script>');
-      }
-
-    });
-
-
-  }, () => {},
-  true
-);
 
 app.use(bodyParser.urlencoded({
   extended: false
