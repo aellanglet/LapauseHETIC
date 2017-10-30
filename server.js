f07a3445fa044cafe4b8505ee0740ccb67ab9ae4
aellanglet <ael.langlet@hetic.net>
2017-10-30 10:52:59 +0100

    Just a cron program

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
   new CronJob('*/5 * * * *', () => {
 
-      if ((new Date().getHours() == "10") && (new Date().getMinutes() == "30")) {
+      if ((new Date().getHours() > "10") && (new Date().getMinutes() > "30")) {
         console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
         console.log("Il est : " + new Date());
         res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '\n<p>Il est : ' + new Date() + '</p>');
