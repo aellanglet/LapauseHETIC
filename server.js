5c03c7e4f2323461a891381e184b53d69cd9e17a
aellanglet <ael.langlet@hetic.net>
2017-10-30 11:59:28 +0100

    Just a cron program

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const CronJob = require('cron').CronJob;
 
 console.log("Le programme est lancé ! " + new Date());
 
+app.get("/", (req, res) => {
+  res.write('<h1>Progamme LaPauseHetic !</h1>' + '<br>' + '<h2>Le programme est lancé ! ' + new Date() + '</h2>');
+
+  if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
+    res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>');
+  } else {
+    res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>');
+  }
+
+});
 
 new CronJob('*/5 * * * *', () => {
 
@@ -22,12 +32,10 @@ new CronJob('*/5 * * * *', () => {
 
     app.get("/", (req, res) => {
 
-      res.write('<h1>Progamme LaPauseHetic !</h1>' + '<br>' + '<h2>Le programme est lancé ! ' + new Date() + '</h2>');
-
       if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
         res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>' + '<script>location.reload()</script>');
       } else {
-        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + location.reload() + '</script>');
+        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<script>location.reload()</script>');
       }
 
     });
