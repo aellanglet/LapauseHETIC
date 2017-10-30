e588edf4be8437a47da5d354ae0b63d86c920292
aellanglet <ael.langlet@hetic.net>
2017-10-30 11:06:57 +0100

    Just a cron program

--- server.js
+++ server.js
@@ -16,19 +16,22 @@ app.get("/", (req, res) => {
       if ((new Date().getHours() > "10") && (new Date().getMinutes() > "30")) {
         console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
         console.log("Il est : " + new Date());
-        res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '\n<p>Il est : ' + new Date() + '</p>');
+        res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>');
+        res.writeHead(303, {
+          Location: req.headers.referer
+        });
+        res.end();
       } else {
         console.log("Ce n'est pas encore l'heure de la pause.");
         console.log("Il est : " + new Date());
-        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '\n<p>Ce n\'est pas encore l\'heure de la pause.</p>');
+        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>');
+        res.writeHead(303, {
+          Location: req.headers.referer
+        });
+        res.end();
       }
 
-    }, () => {
-      res.writeHead(303, {
-        Location: req.headers.referer
-      });
-      res.end();
-    },
+    }, () => {},
     true
   );
 
