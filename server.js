c581b58b99c87dd32dee95e8e70db39df8e18242
aellanglet <ael.langlet@hetic.net>
2017-10-30 11:51:30 +0100

    Just a cron program

--- server.js
+++ server.js
@@ -3,33 +3,43 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser');
+
 const CronJob = require('cron').CronJob;
 
 console.log("Le programme est lancé ! " + new Date());
 
-app.get("/", (req, res) => {
 
-  res.write('<h1>Progamme LaPauseHetic !</h1>' + '\n<h2>Le programme est lancé ! ' + new Date() + '</h2>');
+new CronJob('*/5 * * * *', () => {
 
-  new CronJob('*/5 * * * *', () => {
+    if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
+      console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
+      console.log("Il est : " + new Date());
+    } else {
+      console.log("Ce n'est pas encore l'heure de la pause.");
+      console.log("Il est : " + new Date());
+    }
 
-      if ((new Date().getHours().toString() > "10") && (new Date().getMinutes().toString() > "30")) {
-        console.log("Il est 10 h 30 passé ? C'est déjà l'heure de la pause !");
-        console.log("Il est : " + new Date());
-        res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>');
-        res.redirect(req.originalUrl);
+    app.get("/", (req, res) => {
+
+      res.write('<h1>Progamme LaPauseHetic !</h1>' + '<br>' + '<h2>Le programme est lancé ! ' + new Date() + '</h2>');
+
+      if ((new Date().getHours() > 10) && (new Date().getMinutes() > 30)) {
+        res.write('<p>Il est 10 h 30 passé ? C\'est déjà l\'heure de la pause !</p>' + '<br>' + '<p>Il est : ' + new Date() + '</p>' + '<script>location.reload()</script>');
       } else {
-        console.log("Ce n'est pas encore l'heure de la pause.");
-        console.log("Il est : " + new Date());
-        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>');
-        res.redirect(req.originalUrl);
+        res.write('<p>Ce n\'est pas encore l\'heure de la pause.</p>' + '<br>' + '<p>Ce n\'est pas encore l\'heure de la pause.</p>' + location.reload() + '</script>');
       }
 
-    }, () => {},
-    true
-  );
+    });
 
-});
+
+  }, () => {},
+  true
+);
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 
 app.set('port', (process.env.PORT || 3000));
